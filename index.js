d84298ec0dd6ccbbb0049738c29fb225384e61c4
Maxim <79743489+MaximHTML@users.noreply.github.com>
2021-03-21 23:33:34 +0300

    пасхалка

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var g = document.getElementById("CL");
 var DEVIZ = document.getElementById("DEV");
+var SHL = document.getElementById("SHL");
 function CL() {
     g.innerHTML = "Наш ебанный класс";
 }
 function DEV() {
     DEVIZ.innerHTML = "Наш девиз 4 слова: Пусть горит ебанная школа" ;
+}
+function SH(){
+    SHL.innerHTML = "НЕ крашиха😂" ;
 }
