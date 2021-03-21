1b96985b2e573d46c9c27d06c11a24574f3ac996
Maxim <79743489+MaximHTML@users.noreply.github.com>
2021-03-21 12:47:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var g = document.getElementById("CL");
+var DEVIZ = document.getElementById("DEV");
 function CL() {
     g.innerHTML = "Наш ебанный класс";
+}
+function DEV() {
+    DEVIZ.innerHTML = "Наш девиз 4 слова: Пусть горит ебанная школа" ;
 }
